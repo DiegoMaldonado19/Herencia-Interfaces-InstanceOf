d44e69c3ff3aefc825cd424929c0d6676a43e1a1
Diego Maldonado <dj_maldonado19@hotmail.es>
2021-06-24 14:49:54 -0600

    Generacion de vehiculos aleatoria, programa al 100%

--- src/main/java/vehiculo/VectorVehiculo.java
+++ src/main/java/vehiculo/VectorVehiculo.java
@@ -38,14 +38,16 @@ public class VectorVehiculo {
      * Metodo para inicializar vehiculos
      */
     public void incicializarVehiculos(){
+        int random=0;
         for(int i=0; i<vehiculos.length; i++){
-            if(i>=0 && i<6){
+            random = (int)(Math.random()*20);
+            if(random>=0 && random<6){
                 vehiculos[i] = new VehiculoCamioneta("Diesel",30, 20, 160, 3.5);
             }
-            else if(i>=6 && i<=12){
+            else if(random>=6 && random<=12){
                 vehiculos[i] = new VehiculoCarrera("Gasolina",15, 20, 260, 5.5);
             }
-            else if(i>12 && i<=19){
+            else if(random>12 && random<=19){
                 vehiculos[i] = new VehiculoCarga("Diesel",10, 50, 100, 1.5);
             } 
         }
